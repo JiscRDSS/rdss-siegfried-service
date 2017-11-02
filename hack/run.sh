1265516f55dc9f6e334a2441265fcae05d215b46
Jesús García Crespo <jesus@sevein.com>
2017-11-02 12:59:41 -0700

    Move entry point to cmd package

--- hack/run.sh
+++ hack/run.sh
@@ -8,5 +8,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$( cd "$( dirname "${DIR}" )" && pwd )"
 
 cd ${ROOT}
-go install .
+go install ./cmd/...
 rdss-siegfried-service -home=${ROOT}/siegfried-data -sf=$(which sf)
